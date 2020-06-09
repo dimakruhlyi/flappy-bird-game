0e62d3f0513be5a9c9dbe5c1713b4f0323f12e68
dimakruhlyi <dimakruhly@gmail.com>
2020-06-09 18:33:40 +0300

    created moving animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-const canvas = document.getElementById('canvas');
+window.onload = function(){
+    const canvas = document.getElementById('canvas');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const ctx = canvas.getContext('2d');
+    const background = new Background();
+    gameLoop();
+    //ctx.drawImage(document.getElementById('bird'), 200, 200);
+    ctx.fillStyle = "#fff";
+    function gameLoop(){
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        background.update();
+        background.render(ctx);
+        window.requestAnimationFrame(gameLoop);
+    }
+}
+
+const Background = function(){
+    this.bgPosition = 0;
+    this.fgPosition = 0;
+    this.speed = 10;
+    this.bgImage = document.getElementById('bg');
+}
+
+Background.prototype.update = function(){
+    this.bgPosition -= this.speed;
+}
+
+Background.prototype.render = function(ctx){
+    ctx.drawImage(this.bgImage, this.bgPosition, 0);
+}
